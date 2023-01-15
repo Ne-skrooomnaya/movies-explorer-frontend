3f6fce73374dc6fa73b26d5a0323c212612664c3
ne_skrooomnaya <angelf04032005@gmail.com>
2023-01-15 16:53:56 +0300

    diplom/server-filter

--- src/configs/filter.js
+++ src/configs/filter.js
@@ -1,5 +1,4 @@
 export const movieFilter = (allMovies, film, checkShorts) => {
-  // eslint-disable-next-line array-callback-return
   return allMovies.filter((movieCard) => {
     if (checkShorts === false) {
       return movieCard.nameRU?.toLowerCase().includes(film.toLowerCase());
